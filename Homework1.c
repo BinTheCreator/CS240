bfb51a82a9d3a2eb37c368b20afa0b51455751e8
Nhan Do <nhando591@gmail.com>
2023-09-15 00:23:21 -0400

    Completed Homework 1

---
+++ Homework1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+    char c;
+    float degree, result;
+    
+    printf("Press c to convert temperature from Fahrenheit to Celsius.\n");
+    printf("Press f to convert temperature from Celsius to Fahrenheit.\n");
+    while (c != EOF)
+    {
+        printf("Enter your choice (c, f):");
+        scanf(" %c", &c);
+        
+            if (c == 'c' || c == 'C')
+        {
+            printf("Enter temperature in Celsius:");
+            scanf(" %f", &degree);
+            result = (degree * 9/5) + 32;
+            printf("Temperature in Fahrenheit: %.2f\n", result);
+            break;       
+
+        } else if (c == 'f' || c == 'F')
+        {
+            printf("Enter temperature in Fahrenheit: ");
+            scanf("%f", &degree);
+            result = (degree -32) * 5/9;
+            printf("Temperature in Celsius: %.2f\n", result);
+            break;
+            
+        } else {
+             printf("Please enter a valid input. Try again!\n");
+        }
+
+    } 
+       
+
+}
