a546c3643222fd6622264e01ed3b01d26c0e4775
Nhan Do <nhando591@gmail.com>
2023-10-07 16:45:04 -0400

    Completed Hw 2

---
+++ Homework2/Test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int i = 2; //being used to assign a variable 
+
+    if (1 = i) //being used as an equality operator
+    { 
+        printf("true\n");
+    } else {
+        printf("false\n");
+    }
+   
+}
