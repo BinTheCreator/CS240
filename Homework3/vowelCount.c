683c9939eb3eac9dca7fc8d0a08954b35e7b3a4a
Nhan Do <nhando591@gmail.com>
2023-10-23 16:10:15 -0400

    Create and finished averageScore.c, working on vowelCount

---
+++ Homework3/vowelCount.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+ int main(void)
+ {
+    char string[100];
+    printf("Enter a string\n");
+    fgets(string, sizeof(string), stdin);
+    
+    return 0;
+ }
+ int check_vowel(){ 
+
+ }
